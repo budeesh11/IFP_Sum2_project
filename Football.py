ea72e2bf8dec59551b4a6f56a7fd4e3af93110dc
budeesh11 <pratishselvam4@gmail.com>
2025-04-28 18:01:46 +0100

    Update Football.py

--- Football.py
+++ Football.py
@@ -13,6 +13,17 @@ pygame.mixer.init()
 
 # constants for window
 SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 800
+CARD_WIDTH, CARD_HEIGHT = 120, 160
+CARD_SPACING = 30
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 100, 0)
+RED = (200, 0, 0)
+BLUE = (0, 0, 200)
+GRAY = (200, 200, 200)
+FONT_SIZE = 24
+TURN_TIME = 30  # seconds per turn
+
 
 class game():
     def __init__(self):
@@ -36,11 +47,8 @@ class game():
         pygame.quit()
         sys.exit()
 
-    def draw_cards(self):
-        card_list = ["1S", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS", "AS",
-                     "1H", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH", "AH",
-                     "1C", "2C", "3C", "4C", "5C", "6C", "7C", "8C", "9C", "10C", "JC", "QC", "KC", "AC",
-                     "1D", "2D", "3D", "4D", "5D", "6D", "7D", "8D", "9D", "10D", "JD", "QD", "KD", "AD",]
 
-
+class Card:
+    def card_id(self):
+        card_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
 
