fc8a071186baa80c853b0b3d6173754a7e1896d6
Kkisuu <samkisuu96@gmail.com>
2025-04-28 18:08:14 +0100

    Card object initialization
    Creating the card class with its attributes

--- Football.py
+++ Football.py
@@ -49,6 +49,11 @@ class game():
 
 
 class Card:
-    def card_id(self):
-        card_id = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36]
+    def __init__(self, id, suite, card_value):
+        self.id = id
+        self.suite = suite
+        self.card_value = card_value
+
+
+        pass
 
