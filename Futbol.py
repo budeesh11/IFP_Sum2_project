7a4d380b00ff50933e5f466dfb31e5916c9bc657
budeesh11 <pratishselvam4@gmail.com>
2025-04-28 15:26:33 +0100

    Update Futbol.py

--- Futbol.py
+++ Futbol.py
@@ -1,1 +1,15 @@
+import pygame
+import random
+import sys
+import os
+from typing import List, Tuple, Optional
+from enum import Enum
+import time
 
+# initialising pygame
+
+pygame.init()
+pygame.mixer.init()
+
+# constants for window
+SCREEN_WIDTH, SCREEN_HEIGHT = 1200, 800
