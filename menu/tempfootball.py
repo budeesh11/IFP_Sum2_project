33e781e33d676b6b020de8eab565fc2fb2e1f22e
Clinksys <majeddandari2006@gmail.com>
2025-05-03 18:08:34 +0100

    Update tempfootball.py

--- menu/tempfootball.py
+++ menu/tempfootball.py
@@ -2,35 +2,50 @@
 
 import pygame
 import sys
+from menu import *
 
 class game():
     def __init__(self):
         pygame.init()
         self.running, self.playing = True, False #running and playing state
         self.UP_KEY, self.DOWN_KEY, self.START_KEY, self.BACK_KEY = False, False, False, False #key states
-        self.DISPLAY_W, self.DISPLAY_H = 1200, 800 #canvas size
+        self.DISPLAY_W, self.DISPLAY_H = 1280, 720 #canvas size
         self.display = pygame.Surface((self.DISPLAY_W, self.DISPLAY_H)) #creates a canvas
         self.window = pygame.display.set_mode((self.DISPLAY_W, self.DISPLAY_H)) #shows player what we are drawing
         self.font_name = None  # Using default system font
         self.font = pygame.font.Font(self.font_name, 32) #use default system font with size 32
         self.BLACK, self.WHITE, self.GREEN, self.RED, self.BLUE = (0, 0, 0), (255, 255, 255), (0, 100, 0), (200, 0, 0), (0, 0, 200) #colors
+        # Load and scale background images
+        self.background_image = pygame.image.load("menu/CardBall.png").convert()
+        self.background_image = pygame.transform.scale(self.background_image, (self.DISPLAY_W, self.DISPLAY_H))
+        self.field_image = pygame.image.load("menu/1.png").convert()
+        self.field_image = pygame.transform.scale(self.field_image, (self.DISPLAY_W, self.DISPLAY_H))
+        self.instructions_image = pygame.image.load("menu/Instructions.png").convert()
+        self.instructions_image = pygame.transform.scale(self.instructions_image, (self.DISPLAY_W, self.DISPLAY_H))
+        self.options_image = pygame.image.load("menu/Instructions (1).png").convert()
+        self.options_image = pygame.transform.scale(self.options_image, (self.DISPLAY_W, self.DISPLAY_H))
+        self.options_menu = OptionsMenu(self)
+        self.instructions_menu = InstructionsMenu(self)
+        self.main_menu = MainMenu(self)
+        self.curr_menu = self.main_menu #current menu
 
     def game_loop(self): #performs the game functions
         while self.playing:
-            self.input_check()
+            self.check_events()
             if self.BACK_KEY:
                 self.playing = False #breaks the while loop without turning off the game
-            self.display.fill(self.GREEN) #canvas
-            self.draw_text('Ready to Play?', 60, self.WHITE, self.DISPLAY_W/2, self.DISPLAY_H/2) #display text
+            self.display.fill(self.BLACK) # Clear the screen first
+            self.display.blit(self.field_image, (0, 0)) # Draw the field image
             self.window.blit(self.display, (0, 0)) #to align the display with the window
             pygame.display.update() #updates the screen
             self.reset() #resets the keys
 
     #checks for player input but going through all list of everything a player can input
-    def input_check(self):
+    def check_events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running, self.playing = False, False
+                self.curr_menu.run_display = False #stops current menu from running
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     self.START_KEY = True
