7789bfd3e3eecf2ec6280f33f0a1135a96fd49cc
salavatkhusnutdinov <salavatkhusnutdinovwork@gmail.com>
2025-05-07 08:23:13 +0100

    Improvement in attack process

--- game_engine.py
+++ game_engine.py
@@ -1,13 +1,17 @@
 # This import as I understand gives ability to work with methods and attributes of a Player class
 from player import Player
 from card_state import CardState
+from card import Card
 
 class gameEngine():
-    def __init__(self, player: Player, computer: Player):
+    def __init__(self, player: Player, computer: Player, CARD_SUITS: list, CARD_RANKS: list):
         self.player = player
         self.computer = computer
         self.attacker = player
         self.defender = computer
+        
+        self.CARD_SUITS = CARD_SUITS
+        self.CARD_RANKS = CARD_RANKS
         
         self.attack_card = None
         self.draw_cards_storage = []
@@ -33,20 +37,29 @@ class gameEngine():
         except:
             return "DEBUG: Invalid target on index - " + target_card_index
         
-        
         if target_card_index == 0:
-            if not self._goalkeeper_attack_validity(target_card_index):
-                return
-                
-    
-    #TODO Goalkeeper protection from invalid attack will be probably reworked
-    def _goalkeeper_attack_validity(self, target_index):
-        if target_index == 0:
             for defender_position in [1, 2, 3]:
                 if defender_position < len(self.defender.active_cards):
                     if self.defender.active_cards[defender_position].card_state != CardState.BEATEN:
-                        return False, "Cannot attack goalkeeper until defenders are beaten"
+                        return "DEBUG: Invalid attack. Attack of goalkeeper is impossible until all defenders are beaten"
+    
+    def _resolve_attack_type(self, attacker_card: Card, defender_card: Card):
+        attacker_value = self.CARD_RANKS.index(attacker_card.rank)
+        defender_value = self.CARD_RANKS.index(defender_card.rank)
         
-        return True, "Valid attack"
+        
+        if (attacker_value > defender_value) or (attacker_value == 0 and defender_value == 8):
+            defender_card.card_state = CardState.BEATEN
+            return "Success"
+        elif attacker_value == defender_value:
+            #TODO Draw situation is not completed
+            self.draw_cards_storage.extend([attacker_card, defender_card])
+            return "Draw"
+        else:
+            self.attacker.deck.append(att)
+        
+        
+                
+    
         
     
